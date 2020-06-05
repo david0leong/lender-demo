bd90cdcdd5b963dd7aab324b6be5e3ab95d67d15
David Leong <david0leong@gmail.com>
2020-06-04 18:34:24 -0700

    Add Team model, resource and controller

---
+++ database/seeds/TeamSeeder.php
@@ -0,0 +1,19 @@
+<?php
+
+use App\Team;
+use Illuminate\Database\Seeder;
+
+class TeamSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Team::truncate();
+
+        factory(Team::class, 10)->create();
+    }
+}
